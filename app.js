07e1d289ed5e6b8e8032e74366a095006da2428c
Sergio Front-end deweloper <74537882+Petropavlovskoy@users.noreply.github.com>
2023-08-02 23:00:43 +0300

    Muzik
    Muzik

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const audio = document.querySelector('#audio');
 const btnka = document.querySelector('#btnka');
 
 btnka.addEventListener('click', () => {
-    audio.paused ? audio.play() : audio.pause();
+   audio.paused ? audio.play() : audio.pause();
 });
